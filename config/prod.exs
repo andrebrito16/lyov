9fefb67def4a63ccaa79743e94156f9c47a4ed6b
André Brito <felipe.brito016@gmail.com>
2022-01-02 00:05:04 -0400

    default port set

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :cgnaflightsapi, CgnaflightsapiWeb.Endpoint,
-  url: [host: "lyov.radarbot.xyz", port: 443],
+  url: [host: "lyov.radarbot.xyz", port: 80],
   http: [port: 4000]
 
 # Do not print debug messages in production
